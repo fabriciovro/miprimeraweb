efedd25440581d791e7151d50da6cd918d8c2b43
Fabricio Valentin Rodríguez Otiniano <i202124067@cibertec.edu.pe>
2024-03-18 19:11:07 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//alert("Hola este es mi Javascript");
+
+//let nombre = "Fabricio";
+//nombre = "Maria";
+
+//var nombre1 = "Fabricio";
+
+//const nombre2 = "Fabricio";
+//nombre2 = "Maria";
+
+//console.log (nombre);
+//console.log (nombre1);
+//console.log (nombre2);
